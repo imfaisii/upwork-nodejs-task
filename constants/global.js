83c6c0920b0de35a018912a9b2b80df890301945
Faisal Ashfaq <cfaysal099@gmail.com>
2024-11-27 10:08:42 +0000

    (fix): updated other api calls dependency on countries api call

--- constants/global.js
+++ constants/global.js
@@ -1,6 +1,10 @@
+import env from '../config/env.js';
+
 export const PORT = 3000;
 
 export const RESPONSE = {
     SUCCESS: 'success',
     ERROR: 'error'
 }
+
+export const APP_URL = `${env.APP_URL}:${PORT}`;
